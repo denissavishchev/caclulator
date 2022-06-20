28c08037df04abede49c72fed4f4af33a9bf7a14
Denis Savishchev <77541683+denissavishchev@users.noreply.github.com>
2022-06-20 23:18:32 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,69 @@ void main() {
 class CalculatorApp extends StatefulWidget {
   const CalculatorApp({Key? key}) : super(key: key);
 
+
+
   @override
   State<CalculatorApp> createState() => _CalculatorAppState();
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
 
-  numberPressed(int number) {
-    print(number);
+  int firstNum = 0;
+  int secondNum = 0;
+  String result = '';
+  String textToDisplay = '';
+  String operation = '';
+  String history = '';
+
+
+  buttonPressed(String btnValue) {
+    print(btnValue);
+
+      if (btnValue == 'C'){
+        firstNum = 0;
+        secondNum = 0;
+        result = '';
+        history = '';
+      }
+      else if (btnValue == '+' ||
+                btnValue == '-' ||
+                btnValue == 'X' ||
+                btnValue == '/') {
+        firstNum = int.parse(textToDisplay);
+        result = '';
+        operation = btnValue;
+      }
+      else if (btnValue == '<') {
+        result = textToDisplay.substring(0, textToDisplay.length - 1);
+      }
+      else if (btnValue == '=') {
+        secondNum = int.parse(textToDisplay);
+        if (operation == '+') {
+          result = (firstNum + secondNum).toString();
+          history = firstNum.toString() + operation.toString() + secondNum.toString();
+        }
+        if (operation == '-') {
+          result = (firstNum - secondNum).toString();
+          history = firstNum.toString() + operation.toString() + secondNum.toString();
+        }
+        if (operation == 'X') {
+          result = (firstNum * secondNum).toString();
+          history = firstNum.toString() + operation.toString() + secondNum.toString();
+        }
+        if (operation == '/') {
+          result = (firstNum / secondNum).toString();
+          history = firstNum.toString() + operation.toString() + secondNum.toString();
+        }
+      }else {
+        result = int.parse(textToDisplay + btnValue).toString();
+      }
+      setState(() {
+        textToDisplay = result;
+      });
   }
 
-  operatorPressed(String operator) {
-    print(operator);
-  }
 
-  result() {
-    print('result');
-  }
-
-  clear() {
-    print('Clear');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,75 +109,96 @@ class _CalculatorAppState extends State<CalculatorApp> {
               margin: const EdgeInsets.symmetric(horizontal: 10.0),
               child: Column(children: <Widget>[
                 const SizedBox(height: 40,),
-                const TextField(
-                  enabled: false,
-                  textAlign: TextAlign.right,
-                  decoration: InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                      color: otherButtonsColor,
-                    ),),
-                    // labelText: 'Result'
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  alignment: Alignment.centerRight,
+                  width: double.infinity,
+                  height: 50,
+                  decoration:  BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: otherButtonsColor.withOpacity(0.7),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: Offset(0, 0)),
+                    ],
+                  ),
+                  child: Text(
+                   '$textToDisplay',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 30,
+                    color: Colors.white),
+               ),
+                ),
+                const SizedBox(height: 15,),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  alignment: Alignment.centerRight,
+                  width: double.infinity,
+                  height: 40,
+                  decoration:  BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: otherButtonsColor.withOpacity(0.6),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: Offset(0, 0)),
+                    ],
+                  ),
+                  child: Text(
+                    '$history',
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(fontSize: 20,
+                        color: Colors.white)
                   ),
                 ),
-                const SizedBox(height: 5,),
-                const TextField(
-                  enabled: false,
-                  decoration: InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: otherButtonsColor,
-                      )
-                    ),
-                    isDense: true,
-                    contentPadding: EdgeInsets.all(8),
-                  ),
-                ),
-                const SizedBox(height: 10,),
+                const SizedBox(height: 30,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                  CalcButton(child: ButtonText(text: 'C', color: Colors.white,), onTap: clear, backgroundColor: otherButtonsColor,),
-                  CalcButton(child: ButtonText(text: '<<', color: Colors.white,), onTap: () => operatorPressed('<<'), backgroundColor: otherButtonsColor,),
-                  CalcButton(child: ButtonText(text: '%', color: Colors.white,), onTap: () => operatorPressed('%'), backgroundColor: otherButtonsColor,),
-                  CalcButton(child: ButtonText(text: '/', color: Colors.white,), onTap: () => operatorPressed('/'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: 'C', color: Colors.white,), onTap: () => buttonPressed('C'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: '<', color: Colors.white,), onTap: () => buttonPressed('<'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: '%', color: Colors.white,), onTap: () => buttonPressed('%'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: '/', color: Colors.white,), onTap: () => buttonPressed('/'), backgroundColor: otherButtonsColor,),
                 ],),
                 const SizedBox(height: 10,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                  CalcButton(child: ButtonText(text: '7'), onTap: () => numberPressed(7)),
-                  CalcButton(child: ButtonText(text: '8'), onTap: () => numberPressed(8)),
-                  CalcButton(child: ButtonText(text: '9'), onTap: () => numberPressed(9)),
-                  CalcButton(child: ButtonText(text: 'X', color: Colors.white,), onTap: () => operatorPressed('*'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: '7'), onTap: () => buttonPressed('7')),
+                  CalcButton(child: ButtonText(text: '8'), onTap: () => buttonPressed('8')),
+                  CalcButton(child: ButtonText(text: '9'), onTap: () => buttonPressed('9')),
+                  CalcButton(child: ButtonText(text: 'X', color: Colors.white,), onTap: () => buttonPressed('X'), backgroundColor: otherButtonsColor,),
                   ],),
                 const SizedBox(height: 10,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                  CalcButton(child: ButtonText(text: '4'), onTap: () => numberPressed(4)),
-                  CalcButton(child: ButtonText(text: '5'), onTap: () => numberPressed(5)),
-                  CalcButton(child: ButtonText(text: '6'), onTap: () => numberPressed(6)),
-                  CalcButton(child: ButtonText(text: '-', color: Colors.white,), onTap: () => operatorPressed('-'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: '4'), onTap: () => buttonPressed('4')),
+                  CalcButton(child: ButtonText(text: '5'), onTap: () => buttonPressed('5')),
+                  CalcButton(child: ButtonText(text: '6'), onTap: () => buttonPressed('6')),
+                  CalcButton(child: ButtonText(text: '-', color: Colors.white,), onTap: () => buttonPressed('-'), backgroundColor: otherButtonsColor,),
                   ],),
                 const SizedBox(height: 10,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                  CalcButton(child: ButtonText(text: '1'), onTap: () => numberPressed(1)),
-                  CalcButton(child: ButtonText(text: '2'), onTap: () => numberPressed(2)),
-                  CalcButton(child: ButtonText(text: '3'), onTap: () => numberPressed(3)),
-                  CalcButton(child: ButtonText(text: '+', color: Colors.white,), onTap: () => operatorPressed('+'), backgroundColor: otherButtonsColor,),
+                  CalcButton(child: ButtonText(text: '1'), onTap: () => buttonPressed('1')),
+                  CalcButton(child: ButtonText(text: '2'), onTap: () => buttonPressed('2')),
+                  CalcButton(child: ButtonText(text: '3'), onTap: () => buttonPressed('3')),
+                  CalcButton(child: ButtonText(text: '+', color: Colors.white,), onTap: () => buttonPressed('+'), backgroundColor: otherButtonsColor,),
                   ],),
                 const SizedBox(height: 10,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    CalcButton(child: ButtonText(text: '0'), onTap: () => numberPressed(0)),
-                    CalcButton(child: ButtonText(text: '.'), onTap: () => operatorPressed('.')),
+                    CalcButton(child: ButtonText(text: '0'), onTap: () => buttonPressed('0')),
+                    CalcButton(child: ButtonText(text: '.'), onTap: () => buttonPressed('.')),
                     SizedBox(height: 80,child:
                       CalcButton(child: ButtonText(text: '=', color: Colors.white,),
-                        onTap: result, backgroundColor: otherButtonsColor, size: 173,),),
+                        onTap: () => buttonPressed('='), backgroundColor: otherButtonsColor, size: 173,),),
                   ],)
               ],),
             ),
